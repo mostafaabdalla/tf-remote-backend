1711c5ee573887ea0614ee0eb8c722eca30617ab
mostafa <mostafaa.abdallaa@gmail.com>
2022-02-24 13:41:51 +0000

    added the dynamodb table to the backend configuration

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "<unique_bucket_name>"
-    key    = "remotedemo.tfstate"
-    region = "us-east-1"
+    bucket         = "<unique_bucket_name>"
+    key            = "remotedemo.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "<table_name>" #to enable state locking
   }
 }
